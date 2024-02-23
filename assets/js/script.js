8c5b186e0ee1636f6193833613830d932b5fe72e
Delboy <Derek.degoey@hotmail.co.uk>
2024-02-23 10:31:26 +0000

    Added script that highlights selected bundle

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,21 @@ const sideNavToggle = () => {
 
 hamburger.addEventListener("click", sideNavToggle);
 exit.addEventListener("click", sideNavToggle);
+
+
+// Highlights Selected Bundle 
+const bundles = document.querySelectorAll('.bundle-card');
+
+bundles.forEach(bundle => {
+    bundle.addEventListener('click', () => {
+        clearSelectedBundle();
+        
+        bundle.setAttribute('data-selected', 'true')
+    })
+})
+
+const clearSelectedBundle = () => {
+    bundles.forEach(bundle => {
+        bundle.setAttribute('data-selected', 'false')
+    })
+}
